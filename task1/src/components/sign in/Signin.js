f444b340ab139b1f8b2679db6453c24ec1eb7412
shashank lalit patil <shashankpatil125@gmail.com>
2022-07-23 16:15:10 +0000

    second ccommit

--- task1/src/components/sign in/Signin.js
+++ task1/src/components/sign in/Signin.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import dark from "/workspace/ui/task1/src/components/image/dark.jpg"
 import light from '/workspace/ui/task1/src/components/image/light.jpg'
+import google from '/workspace/ui/task1/src/components/image/google.png'
+import fb from '/workspace/ui/task1/src/components/image/facebook.png'
 import { Icon } from '@iconify/react';
 
 function background() {
   return (
 
     <div className='w-screen h-screen bg-no-repeat bg-cover' style={{ backgroundImage: `url(${dark})` }}>
-      <div class="flex h-screen justify-center items-center">
-        <div class="text-center ">
-          <h1 class="text-3xl font-font-mast text-6xl text-white mb-8">Sign in</h1>
-          <p class="text-xl text-white mb-3 text-2xl">Sign in and get started</p>
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center ">
+          <h1 className=" font-font-mast text-6xl text-white mb-8">Sign in</h1>
+          <p className="text-white mb-3 text-2xl">Sign in and get started</p>
           <div className='w-80'>
             <input type="text" placeholder='Login' className="w-full rounded-lg h-10 text-xl pl-6 mb-4 text-white bg-sky-900" ></input>
             <br />
@@ -20,7 +22,14 @@ function background() {
               <p className='text-white'>Remember me</p>
               <p className='text-green-400  absolute right-0'>Forgot Password</p>
             </div>
-            <button className=" text-white w-full mt-4 rounded-lg h-10 text-xl text-center bg-green-600 mb-4">Login</button>
+
+            <div className='flex justify-center mt-3'>
+              <img src={google} alt="google" className='w-10 rounded-full mx-2'></img>
+              <img src={fb} alt="fb" className='w-10 rounded-full  mx-2'></img>
+            </div>
+
+
+            <button className=" text-white w-full mt-4 rounded-lg h-10 text-xl text-center bg-green-600 mb-10 ">Login</button>
           </div>
         </div>
       </div>
@@ -31,10 +40,10 @@ function background() {
 function lightground() {
   return (
     <div className='w-screen h-screen bg-no-repeat bg-cover' style={{ backgroundImage: `url(${light})` }}>
-      <div class="flex h-screen justify-center items-center">
-        <div class="text-center ">
-          <h1 class="text-3xl font-font-mast text-6xl text-black mb-8">Sign in</h1>
-          <p class="text-xl text-white mb-3 text-2xl text-black">Sign in and get started</p>
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center ">
+          <h1 className="font-font-mast text-6xl text-black mb-8">Sign in</h1>
+          <p className=" mb-3 text-2xl text-black">Sign in and get started</p>
           <div className='w-80'>
             <input type="text" placeholder='Login' className="w-full rounded-lg h-10 text-xl pl-6 mb-4 text-white bg-sky-900" ></input>
             <br />
@@ -44,7 +53,13 @@ function lightground() {
               <p className='text-gray-600'>Remember me</p>
               <p className='text-black  absolute right-0'>Forgot Password</p>
             </div>
-            <button className="w-full mt-4 rounded-lg h-10 text-xl text-center bg-green-600 mb-4">Login</button>
+            
+            <div className='flex justify-center mt-3'>
+              <img src={google} alt="google" className='w-10 rounded-full mx-2'></img>
+              <img src={fb} alt="fb" className='w-10 rounded-full  mx-2'></img>
+            </div>
+
+            <button className="w-full mt-4 rounded-lg h-10 text-xl text-center bg-green-600 mb-10">Login</button>
           </div>
         </div>
       </div>
