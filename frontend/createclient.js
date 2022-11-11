8b5f6491498290501d18a0149af676b68d618393
shashank lalit patil <shashankpatil125@gmail.com>
2022-11-11 11:33:26 +0000

    removed frontend and copyed moved inner connet to outside to it

--- frontend/createclient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(
-    'https://ilmdokwizfwrxisiejiq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsbWRva3dpemZ3cnhpc2llamlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk5NTQwNTEsImV4cCI6MTk3NTUzMDA1MX0.55cyt865pWsMi3p5z5RoKLKICPGb5ehYeJJHtjGABG0'
-)
--- frontend/src/components/registration/Confirm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Confirm() {
-  return (
-    <div className=''></div>
-  )
-}
-
-export default Confirm
